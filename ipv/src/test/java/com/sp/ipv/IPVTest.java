a9b630aad48ee3d45171c97e43f992a2b18e6f0c
Juan Carlos Hernandez Iñiguez <juanhernandez@99.200-57-1.bestel.com.mx>
2018-10-15 11:06:39 -0500

    adding runner

---
+++ ipv/src/test/java/com/sp/ipv/IPVTest.java
@@ -0,0 +1,15 @@
+package com.sp.ipv;
+
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        snippets = SnippetType.CAMELCASE,
+        plugin = "html:out"
+        )
+public class IPVTest {
+}
