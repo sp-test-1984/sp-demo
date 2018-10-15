4eea860a29e8cb1a26d9310a529f8cdf0aea4566
Juan Carlos Hernandez Iñiguez <juanhernandez@Juans-MacBook-Pro-2.local>
2018-10-15 12:59:06 -0500

    modified runner to produce html

--- ipv/src/test/java/com/sp/ipv/IPVTest.java
+++ ipv/src/test/java/com/sp/ipv/IPVTest.java
@@ -9,7 +9,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         snippets = SnippetType.CAMELCASE,
-        plugin = "html:out"
+        plugin = "html:out",
+        features = "src/test/resources"
         )
 public class IPVTest {
 }
